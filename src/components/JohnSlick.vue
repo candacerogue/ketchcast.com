4921e30c599d852ab0b24ea02e651546bae28109
Candace <candacerogers114@gmail.com>
2020-10-26 13:57:09 -0700

    Calling images locally to fixed mixed content error

--- src/components/JohnSlick.vue
+++ src/components/JohnSlick.vue
@@ -4,7 +4,7 @@
     <transition-group name="fade" tag="div">
       <div v-for="i in [currentIndex]" :key="i">
         <a :href="currentImg.href" target="_blank"> 
-          <img :src="currentImg.src"  :alt="currentImg.alt" />
+          <img :src="getImgUrl(currentImg.src)"  :alt="currentImg.alt" />
         </a>
       </div>
     </transition-group>
@@ -18,47 +18,47 @@
       return {
         images: [
           {
-             src: 'http://vps49371.inmotionhosting.com/~yerr/images/Art-1-Pic.png',
+             src: 'Art-1-Pic',
              alt: "Julia Collins Transformed The Food Industry Now She's Trying to Save the World",
              href: "https://afrotech.com/julia-collins-transformed-the-food-industry-now-shes-trying-to-save-the-world"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-2-Pic.png",
+            src: "Art-2-Pic",
             alt: "Everette Taylor Wants to Disrupt the Art Industry",
             href: "https://afrotech.com/everette-taylor-wants-to-disrupt-the-art-industry"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-3-Pic.png",
+            src: "Art-3-Pic",
             alt: "Herbie Hancock on a Path to Find my Own Answer",
             href: "https://www.npr.org/2014/10/23/354871509/herbie-hancock-on-a-path-to-find-my-own-answer"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-4-Pic.png",
+            src: "Art-4-Pic",
             alt: "Richelieu Dennis Wants to Turn Madam CJ Walkers House into an Incubator for Black Women Entrepreneurs",
             href: "https://afrotech.com/richelieu-dennis-wants-to-turn-madam-c-j-walkers-house-into-an-incubator-for-black-women-entrepreneurs"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-5-Pic.png",
+            src: "Art-5-Pic",
             alt: "Nipsey Hussle the Stem Advocate",
             href: "https://afrotech.com/nipsey-hussle-the-stem-advocate"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-6-Pic.png",
+            src: "Art-6-Pic",
             alt: "A project",
             href: "http://brokenplate.org/"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-7-Pic.png",
+            src: "Art-7-Pic",
             alt: "Director Gina Prince Bythewood Its Time to Obliterate the Term Black Film",
             href: "https://www.npr.org/sections/codeswitch/2014/11/14/363793023/director-gina-prince-bythewood-its-time-to-obliterate-the-term-black-film"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-8-Pic.png",
+            src: "Art-8-Pic",
             alt: "Susan Rice Islamic State Fight Will Not Be Irage War Redux",
             href: "https://www.npr.org/sections/thetwo-way/2014/09/11/347746868/susan-rice-islamic-state-fight-will-not-be-iraq-war-redux"
           },
           {
-            src: "http://vps49371.inmotionhosting.com/~yerr/images/Art-9-Pic.png",
+            src: "Art-9-Pic",
             alt: "What is it about Kendrick Lamar",
             href: "https://www.npr.org/2014/10/28/359340269/what-is-it-about-kendrick-lamar"
           }
@@ -81,6 +81,10 @@
       },
       prev: function() {
         this.currentIndex -= 1;
+      },
+      getImgUrl(pic){
+        let images = require.context('../assets/', false, /\.png$/)
+        return images('./' + pic + ".png")
       }
     },
 
